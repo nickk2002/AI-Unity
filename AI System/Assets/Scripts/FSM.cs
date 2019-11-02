eb5052395bfcbc20ee9146fe449a9d1a657766fa
nickk2002 <nicolae.filat@yahoo.com>
2019-11-02 19:25:55 +0200

    modificari ca sa mearga totul bine

--- AI System/Assets/Scripts/FSM.cs
+++ AI System/Assets/Scripts/FSM.cs
@@ -20,7 +20,7 @@ public class FSM : MonoBehaviour
 
     [SerializeField] private NavMeshAgent navMeshAgent;
 
-    private bool newState = false;
+    private Vector3 playerPosition;
 
     private void Awake()
     {
@@ -29,21 +29,47 @@ public class FSM : MonoBehaviour
     private void Update()
     {
         Debug.Log(curentState);
+
+        playerPosition = navMeshAgent.gameObject.transform.position;
+
         switch (curentState)
         {
             case State.pos1:
-                inputCommandObject.Destination = pos1.position;
-                Debug.Log(navMeshAgent.destination);
-                if (navMeshAgent.remainingDistance < 1)
+                if (!navMeshAgent.pathPending)
+                {
+                    inputCommandObject.Destination = pos1.position;
+                   
+                }
+                if (Vector3.Distance(playerPosition,pos1.position) < 2f)
+                {
+                    Debug.Log(navMeshAgent.destination);
+                    Debug.Log(navMeshAgent.gameObject.transform.position);
                     Debug.Log("Am ajuns");
                     curentState = State.change_color1;
+                }
                 break;
             case State.change_color1:
                 Color color = new Color(Random.value, Random.value, Random.value);
                 inputCommandObject.DesiredColor = color;
                 curentState = State.pos2;
                 break;
+            case State.change_color2:
+                color = new Color(Random.value, Random.value, Random.value);
+                inputCommandObject.DesiredColor = color;
+                curentState = State.pos1;
+                break;
+            case State.pos2:
+                if (!navMeshAgent.pathPending)
+                {
+                    inputCommandObject.Destination = pos2.position;
 
+                }
+                if (Vector3.Distance(playerPosition, pos2.position) < 2f)
+                {
+                    Debug.Log("Am ajuns");
+                    curentState = State.change_color2;
+                }
+                break;
         }
     }
 }
